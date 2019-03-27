6be35a7f05bf4dfcb3ec390a2547494d699eaff5
brayanlopez <balg.lg55@gmail.com>
2019-03-26 21:46:48 -0500

    Add files via upload

--- Primer taller/script.js
+++ Primer taller/script.js
@@ -1,14 +1,22 @@
+var contador;
+
 function burbuja(arreglo, tamano) {
     var ordenado = arreglo;
+    contador = 0;
+    contador += 1;
     for (var i = 0; i <= tamano - 1; i++) {
+        contador += 5;
         for (var j = i + 1; j < tamano; j++) {
             if (ordenado[i] > ordenado[j]) {
+                contador += 7;
                 var temporal = ordenado[i];
                 ordenado[i] = ordenado[j];
                 ordenado[j] = temporal;
             }
         }
+//        contador += 1;
     }
+  //  contador += 2;
     return ordenado;
 }
 ;
@@ -27,7 +35,7 @@ function burbujaDescendente(arreglo, tamano) {
     return ordenado;
 }
 ;
-
+/*
 function generarVector(tamano) {
     var vector = [10];
     for (var i = 0; i < tamano; i++) {
@@ -36,23 +44,34 @@ function generarVector(tamano) {
     return vector;
 }
 ;
+*/
+
+function generarVector(tamano) {
+    var vector = [10];
+    for (var i = 0; i < tamano; i++) {
+        vector[i] = 100-i;
+    }
+    return vector;
+}
+;
 
 //6n^2-3
 
 function init() {
-    var arreglo = generarVector(5);
+    var tamano = 10;
+    var arreglo = generarVector(tamano);
     var generar = document.getElementById("generar");
     var vector = document.getElementById("vector");
     var porContador = document.getElementById("porContador");
     var porFormula = document.getElementById("porFormula");
 
 //    arreglo = burbujaDescendente(arreglo,5);
-    arreglo = burbuja(arreglo, 5);
+    arreglo = burbuja(arreglo, tamano);
 
     generar.onclick = function (e) {
         vector.textContent = arreglo;
-        porContador.textContent = 1;
-        porFormula.textContent = 2;
+        porContador.textContent = contador;
+        porFormula.textContent = Math.pow(6, 2) * tamano - 3;
     };
 }
 ;
