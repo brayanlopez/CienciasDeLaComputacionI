c2e8e54c9a28d3cdbed47cff3de5d99e764eaf5c
brayanlopez <balg.lg55@gmail.com>
2019-03-26 21:31:59 -0500

    Add files via upload

--- Primer taller/script.js
+++ Primer taller/script.js
@@ -13,6 +13,21 @@ function burbuja(arreglo, tamano) {
 }
 ;
 
+function burbujaDescendente(arreglo, tamano) {
+    var ordenado = arreglo;
+    for (var i = 0; i <= tamano - 1; i++) {
+        for (var j = i + 1; j < tamano; j++) {
+            if (ordenado[i] < ordenado[j]) {
+                var temporal = ordenado[i];
+                ordenado[i] = ordenado[j];
+                ordenado[j] = temporal;
+            }
+        }
+    }
+    return ordenado;
+}
+;
+
 function generarVector(tamano) {
     var vector = [10];
     for (var i = 0; i < tamano; i++) {
@@ -22,14 +37,22 @@ function generarVector(tamano) {
 }
 ;
 
-function init() { 
+//6n^2-3
+
+function init() {
     var arreglo = generarVector(5);
-    arreglo = burbuja(generarVector(5),5);
     var generar = document.getElementById("generar");
     var vector = document.getElementById("vector");
+    var porContador = document.getElementById("porContador");
+    var porFormula = document.getElementById("porFormula");
+
+//    arreglo = burbujaDescendente(arreglo,5);
+    arreglo = burbuja(arreglo, 5);
 
     generar.onclick = function (e) {
         vector.textContent = arreglo;
+        porContador.textContent = 1;
+        porFormula.textContent = 2;
     };
 }
 ;
